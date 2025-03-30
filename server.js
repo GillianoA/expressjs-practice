039753afdec0adadf4eb4b7f459fb73b386a6bd3
GiillianoA <agard.gilliano@gmail.com>
2025-03-30 10:16:06 -0400

    feat: add user routes and integrate EJS view engine

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('index', {text: 'World'})
 })
+
+const usersRouter = require('./routes/users')
+
+app.use('/users', usersRouter)
 
 app.listen(3000)
